e92778027f21473f78aed6d6f6318876d3281950
chipsorz <15228930218@163.com>
2024-12-03 11:17:14 +0800

    Add files via upload

--- WEB/叮叮书店/js/main.js
+++ WEB/叮叮书店/js/main.js
@@ -27,3 +27,36 @@ function showtime(){
     time += datetime.getSeconds() < 10 ? `0${datetime.getSeconds()}` : `${datetime.getSeconds()}`;
     document.getElementById("date-time").innerHTML = `${date} ${time}`;
 }
+
+var count = 2;
+var a = [...document.querySelectorAll('#banner a')];
+for(let i = 0; i < a.length; i++){
+    a[i].addEventListener('click', function(){
+        count = i + 1;
+        changebgcolor(count);
+    },false);
+}
+function carousel(){
+    let imgSrc = `../叮叮书店素材/图片素材/b-ad${count}.jpg`;
+    let aChange = `a${count}`;
+    // 记录<a>标签需要变回背景颜色的序号
+    let aCount = count - 1;
+    if(aCount == 0) {aCount = 5;}
+    let aRestore = `a${aCount}`;
+    document.getElementById("b-ad").src = imgSrc;
+    document.getElementById(aChange).style.backgroundColor = "hsl(150, 40%, 30%)";
+    document.getElementById(aRestore).style.backgroundColor = "hsl(85, 55%, 50%)";
+    count++;
+    if(count == 6) {count = 1;}
+}
+function changebgcolor(num){
+    document.getElementById("b-ad").src = `../叮叮书店素材/图片素材/b-ad${num}.jpg`;
+    for(let targetNum = 1; targetNum <= 5; targetNum++){
+        let aTarget = `a${targetNum}`;
+        if(targetNum == num){
+            document.getElementById(aTarget).style.backgroundColor = "hsl(150, 40%, 30%)";
+        }else{
+            document.getElementById(aTarget).style.backgroundColor = "hsl(85, 55%, 50%)";
+        }
+    }
+}
